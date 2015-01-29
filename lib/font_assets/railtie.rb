22f213db0aed0dc296110d46214a5e6c7c3eb0cd
Tim Banks <tbanks@rednovalabs.com>
2015-01-29 14:09:59 -0600

    Fix conditional logic

--- lib/font_assets/railtie.rb
+++ lib/font_assets/railtie.rb
@@ -7,7 +7,7 @@ module FontAssets
     initializer "font_assets.configure_rails_initialization" do |app|
       config.font_assets.origin ||= "*"
       config.font_assets.options ||= { allow_ssl: true }
-      config.font_assets.serve_static_assets = app.config.serve_static_assets || true
+      config.font_assets.serve_static_assets = app.config.serve_static_assets.nil? ? true : app.config.serve_static_assets
 
       insert_target = if defined?(ActionDispatch::Static) && config.font_assets.serve_static_assets
         'ActionDispatch::Static'
